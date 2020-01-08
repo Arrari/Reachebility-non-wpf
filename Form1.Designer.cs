7a203e0ffdafe9774508bcc7072d93d6b8f3cb88
Алексей Хоруженко <Voxine@outlook.com>
2020-01-08 15:09:02 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Reachebility_non_wpf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.nudCount = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(230, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(790, 735);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // nudCount
+            // 
+            this.nudCount.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.nudCount.Location = new System.Drawing.Point(12, 43);
+            this.nudCount.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nudCount.Name = "nudCount";
+            this.nudCount.Size = new System.Drawing.Size(120, 20);
+            this.nudCount.TabIndex = 0;
+            this.nudCount.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Radius Aproxim";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1032, 759);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudCount);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown nudCount;
+        private System.Windows.Forms.Label label1;
+    }
+
+}
---
+++ Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Reflection;
+using System.Linq;
+using System.Windows.Forms;
+using System.Globalization;
+using ClipperLib;
+
+
+
+namespace Reachebility_non_wpf
+{
+
+    using Polygon = List<IntPoint>;
+    using Polygons = List<List<IntPoint>>;
+    public partial class Form1 : Form
+    {
+        private float scale = 100;
+        private Bitmap mybitmap;
+        private Polygons subjects = new Polygons();
+        private Polygons clips = new Polygons();
+        private Polygons solution = new Polygons();
+        class SVGBuilder
+        {
+            public class StyleInfo
+            {
+                public PolyFillType pft;
+                public Color brushClr;
+                public Color penClr;
+                public double penWidth;
+                public int[] dashArray;
+                public Boolean showCoords;
+                public StyleInfo Clone()
+                {
+                    StyleInfo si = new StyleInfo();
+                    si.pft = this.pft;
+                    si.brushClr = this.brushClr;
+                    si.dashArray = this.dashArray;
+                    si.penClr = this.penClr;
+                    si.penWidth = this.penWidth;
+                    si.showCoords = this.showCoords;
+                    return si;
+                }
+                public StyleInfo()
+                {
+                    pft = PolyFillType.pftNonZero;
+                    brushClr = Color.AntiqueWhite;
+                    dashArray = null;
+                    penClr = Color.Black;
+                    penWidth = 0.8;
+                    showCoords = false;
+                }
+            }
+
+            public class PolyInfo
+            {
+                public Polygons polygons;
+                public StyleInfo si;
+            }
+
+            public StyleInfo style;
+            private List<PolyInfo> PolyInfoList;
+            const string svg_header = "<?xml version=\"1.0\" standalone=\"no\"?>\n" +
+              "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.0//EN\"\n" +
+              "\"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\">\n\n" +
+              "<svg width=\"{0}px\" height=\"{1}px\" viewBox=\"0 0 {2} {3}\" " +
+              "version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n\n";
+            const string svg_path_format = "\"\n style=\"fill:{0};" +
+                " fill-opacity:{1:f2}; fill-rule:{2}; stroke:{3};" +
+                " stroke-opacity:{4:f2}; stroke-width:{5:f2};\"/>\n\n";
+
+            public SVGBuilder()
+            {
+                PolyInfoList = new List<PolyInfo>();
+                style = new StyleInfo();
+            }
+
+            public void AddPolygons(Polygons poly)
+            {
+                if (poly.Count == 0) return;
+                PolyInfo pi = new PolyInfo();
+                pi.polygons = poly;
+                pi.si = style.Clone();
+                PolyInfoList.Add(pi);
+            }
+
+            public Boolean SaveToFile(string filename, double scale = 1.0, int margin = 10)
+            {
+                if (scale == 0) scale = 1.0;
+                if (margin < 0) margin = 0;
+
+                //calculate the bounding rect ...
+                int i = 0, j = 0;
+                while (i < PolyInfoList.Count)
+                {
+                    j = 0;
+                    while (j < PolyInfoList[i].polygons.Count &&
+                        PolyInfoList[i].polygons[j].Count == 0) j++;
+                    if (j < PolyInfoList[i].polygons.Count) break;
+                    i++;
+                }
+                if (i == PolyInfoList.Count) return false;
+                IntRect rec = new IntRect();
+                rec.left = PolyInfoList[i].polygons[j][0].X;
+                rec.right = rec.left;
+                rec.top = PolyInfoList[0].polygons[j][0].Y;
+                rec.bottom = rec.top;
+
+                for (; i < PolyInfoList.Count; i++)
+                {
+                    foreach (Polygon pg in PolyInfoList[i].polygons)
+                        foreach (IntPoint pt in pg)
+                        {
+                            if (pt.X < rec.left) rec.left = pt.X;
+                            else if (pt.X > rec.right) rec.right = pt.X;
+                            if (pt.Y < rec.top) rec.top = pt.Y;
+                            else if (pt.Y > rec.bottom) rec.bottom = pt.Y;
+                        }
+                }
+
+                rec.left = (Int64)(rec.left * scale);
+                rec.top = (Int64)(rec.top * scale);
+                rec.right = (Int64)(rec.right * scale);
+                rec.bottom = (Int64)(rec.bottom * scale);
+                Int64 offsetX = -rec.left + margin;
+                Int64 offsetY = -rec.top + margin;
+
+                using (StreamWriter writer = new StreamWriter(filename))
+                {
+                    writer.Write(svg_header,
+                        (rec.right - rec.left) + margin * 2,
+                        (rec.bottom - rec.top) + margin * 2,
+                        (rec.right - rec.left) + margin * 2,
+                        (rec.bottom - rec.top) + margin * 2);
+
+                    foreach (PolyInfo pi in PolyInfoList)
+                    {
+                        writer.Write(" <path d=\"");
+                        foreach (Polygon p in pi.polygons)
+                        {
+                            if (p.Count < 3) continue;
+                            writer.Write(String.Format(NumberFormatInfo.InvariantInfo, " M {0:f2} {1:f2}",
+                                (double)((double)p[0].X * scale + offsetX),
+                                (double)((double)p[0].Y * scale + offsetY)));
+                            for (int k = 1; k < p.Count; k++)
+                            {
+                                writer.Write(String.Format(NumberFormatInfo.InvariantInfo, " L {0:f2} {1:f2}",
+                                (double)((double)p[k].X * scale + offsetX),
+                                (double)((double)p[k].Y * scale + offsetY)));
+                            }
+                            writer.Write(" z");
+                        }
+
+                        writer.Write(String.Format(NumberFormatInfo.InvariantInfo, svg_path_format,
+                        ColorTranslator.ToHtml(pi.si.brushClr),
+                        (float)pi.si.brushClr.A / 255,
+                        (pi.si.pft == PolyFillType.pftEvenOdd ? "evenodd" : "nonzero"),
+                        ColorTranslator.ToHtml(pi.si.penClr),
+                        (float)pi.si.penClr.A / 255,
+                        pi.si.penWidth));
+
+                        if (pi.si.showCoords)
+                        {
+                            writer.Write("<g font-family=\"Verdana\" font-size=\"11\" fill=\"black\">\n\n");
+                            foreach (Polygon p in pi.polygons)
+                            {
+                                foreach (IntPoint pt in p)
+                                {
+                                    Int64 x = pt.X;
+                                    Int64 y = pt.Y;
+                                    writer.Write(String.Format(
+                                        "<text x=\"{0}\" y=\"{1}\">{2},{3}</text>\n",
+                                        (int)(x * scale + offsetX), (int)(y * scale + offsetY), x, y));
+
+                                }
+                                writer.Write("\n");
+                            }
+                            writer.Write("</g>\n");
+                        }
+                    }
+                    writer.Write("</svg>\n");
+                }
+                return true;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            mybitmap = new Bitmap(
+            pictureBox1.ClientRectangle.Width,
+            pictureBox1.ClientRectangle.Height,
+            PixelFormat.Format32bppArgb);
+
+        }
+        private void bRefresh_Click(object sender, EventArgs e)
+        {
+            DrawBitmap();
+        }
+        static private PointF[] PolygonToPointFArray(Polygon pg, float scale)
+        {
+            PointF[] result = new PointF[pg.Count];
+            for (int i = 0; i < pg.Count; ++i)
+            {
+                result[i].X = (float)pg[i].X / scale;
+                result[i].Y = (float)pg[i].Y / scale;
+            }
+            return result;
+        }
+        private IntPoint GenerateRandomPoint(int l, int t, int r, int b, Random rand)
+        {
+            int Q = 10;
+            return new IntPoint(
+              Convert.ToInt64((rand.Next(r / Q) * Q + l + 10) * scale),
+              Convert.ToInt64((rand.Next(b / Q) * Q + t + 10) * scale));
+        }
+        private void GenerateRandomPolygon(int count)
+        {
+            int Q = 10;
+            Random rand = new Random();
+            int l = 10;
+            int t = 10;
+            int r = (pictureBox1.ClientRectangle.Width - 20) / Q * Q;
+            int b = (pictureBox1.ClientRectangle.Height - 20) / Q * Q;
+
+            subjects.Clear();
+            clips.Clear();
+
+            Polygon subj = new Polygon(count);
+            for (int i = 0; i < count; ++i)
+                subj.Add(GenerateRandomPoint(l, t, r, b, rand));
+            subjects.Add(subj);
+
+            Polygon clip = new Polygon(count);
+            for (int i = 0; i < count; ++i)
+                clip.Add(GenerateRandomPoint(l, t, r, b, rand));
+            clips.Add(clip);
+
+        }
+
+
+        private void DrawBitmap(bool justClip = false)
+        {
+            //затуп             
+
+            pictureBox1.Image = mybitmap;
+        }
+        private void nudCount_ValueChanged(object sender, EventArgs e)
+        {
+            DrawBitmap(true);
+        }
+    }
+}
+
+
+
+ 
+          
